a4fea20ef0091f3660618e6625282c3874aebd68
Z.M. Vig <zvig@umd.edu>
2025-06-15 16:21:58 -0400

    Updated pyproject.toml

--- spectralops/__init__.py
+++ spectralops/__init__.py
@@ -1,5 +1,13 @@
 # spectralops/__init__.py
 
+"""
+spectralops
+---
+
+Processing operations for individual spectra and spectral image cubes in
+Python.
+"""
+
 from .Spectrum import Spectrum
 from .SpectralCube import SpectralCube
 
